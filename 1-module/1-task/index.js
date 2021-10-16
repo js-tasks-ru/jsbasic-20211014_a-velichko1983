2a9189295baad1aa97d91f0d4fff56be4a03f456
OleksandrVelychko <OleksandrVelychko@users.noreply.github.com>
2021-10-17 00:18:19 +0300

    added solution for lesson 1: factorial function with while loop

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1
+  if (n > 1) {
+    while (n) {
+      result *= n;
+      n--; 
+  }
+  return result
+  }
+  return 1
 }
+
+alert(factorial(1))
